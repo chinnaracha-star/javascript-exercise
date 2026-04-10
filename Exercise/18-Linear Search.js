195619f34318967e3c1615f66f14a962bcba6e91
Chindurex <chinnaracha@gmail.com>
2026-04-10 10:27:26 +0700

    Up Answer Linear Search

--- Exercise/18-Linear Search.js
+++ Exercise/18-Linear Search.js
@@ -2,16 +2,16 @@
 // หากมีให้เอา Index ของ 7 มาแสดงบนหน้าจอ เช่น
 
 // ถ้า numbers = [3, 1, 7, 9, 2] ตัว Algorithm จะแสดงผลลัพธ์ออกมาเป็น 2
-// ถ้า numbers = [2, 10, 1, 9, 20, 15] ตัว Algorithm จะแสดงผลลัพธ์ออกมาเป็น -1
+// ถ้า numbers = [2, 10, 1, 7, 9, 20, 15] ตัว Algorithm จะแสดงผลลัพธ์ออกมาเป็น -1
+ numbers = [2, 10, 1, 7, 9, 20, 15]
+ let inex = -1;
+for (let i = 0; i <= numbers.length; i+=1){
+    if (numbers[i] === 7){
+        inex = i;
+    }
+}
+console.log(inex)
 
-// numbers = [3, 1, 7, 9, 2,]
-// let index = -1;
-// for (let i = 0 ; i < numbers.length ; i++) {
-//     if (numbers[i] === 7) {
-//         index = i;
-//     }
-// }
-// console.log(index);
 //--------------------------------------------------------------------------------------------------------------
 // เรามี Array cities ซึ่งบรรจุชื่อเมือง และมี Variable searchCity เก็บค่าเมืองที่ต้องการค้นหา 
 // ให้เขียน Algorithm ที่ค้นหาว่า cities มี searchCity อยู่หรือไม่ หากมีให้เอา Index ของ searchCity 
@@ -22,12 +22,32 @@
 // ถ้า cities = ["Mumbai", "Bangkok", "Sydney", "Beijing"] และ searchCity = "Rome" 
 // ตัว Algorithm จะแสดงผลลัพธ์ออกมาเป็น -1
 
-cities = ["Sydney", "Paris", "London", "Tokyo", "New York"]
-let searchCity = "Tokyo"   //เพิ่มเพื่อค้นหา
-let index = -1;
-for (let i = 0 ; i < cities.length ; i++)
-    if (cities[i] === searchCity){
-        index = i ;
-    }
+ cities = ["Sydney", "Paris", "London", "Tokyo", "New York"]
+ let inde = -1;
+ let searchCity = "Tokyo";
 
-console.log(index);
+ for (let i = 0; i <= cities.length; i+=1){
+        if(cities[i] === searchCity){
+            inde = i;
+        }
+ }
+console.log(inde)
+//--------------------------------------------------------------------------------------------------------------
+// เรามี Array fruits ซึ่งบรรจุรายชื่อผลไม้
+// ให้เขียน Algorithm ที่ค้นหาว่า fruits มีคำว่า "pear" อยู่หรือไม่
+// หากมีให้เอา Index ของ "pear" มาแสดงบนหน้าจอ
+// เช่น ถ้า fruits = ["apple", "banana", "pear", "orange", "mango"]
+// ตัว Algorithm จะแสดงผลลัพธ์ออกมาเป็น 2
+//           <<<<<วิธีคิด>>>>>>
+// <ในการวนแต่ละครั้ง ให้ใช้ If-Else
+//     <ถ้าสมาชิกตัวนั้น === "pear" 
+//             ให้ทำการ Reassign ค่าให้ index มีค่าเท่ากับตำแหน่งที่พบ>>
+
+fruits = ["apple", "banana", "pear", "orange", "mango"]
+let indx = -1;
+for (let i = 0; i <= fruits.length; i+=1){
+    if (fruits[i] === "pear"){
+        indx = i;
+    }
+}
+console.log(indx)
--- Exercise/Object.js
+++ Exercise/Object.js
@@ -1,18 +1,195 @@
-let thailand = {};
+// let thailand = {};
 
-let A = "region";
-let B = "population";
-thailand[A] = "Southeast Asia";
-console.log(thailand); 
-thailand[B] = 69.8;
-console.log(thailand);
+// let A = "region";
+// let B = "population";
+// thailand[A] = "Southeast Asia";
+// console.log(thailand); 
+// thailand[B] = 69.8;
+// console.log(thailand);
+//-----------------------------------------------------------------------------------------------
+// let AA = {};
+// let b = "names";
+// let a = "name";
 
+// AA.a = "Chin";        //ตัว Key จะเปลี่ยนเป็น a 
+// console.log(AA);
+// AA[b] = "S"           //ตัว Key  จะไม่เปลี่ยน 
+// console.log(AA);
+//-----------------------------------------------------------------------------------------------
+// const dish1 = {
+//   name: "Korma",
+//   type: "Main Course",
+//   country: "India",
+//   spicy: true
+// };
 
+// const dish2 = {
+//   name: "Mango Sticky Rice",
+//   type: "Dessert",
+//   country: "Thailand",
+//   spicy: false
+// };
 
-let AA = {};
+// /*Start coding here 🤩*/
+// let a = "vegetarian"
 
-let a = "EE" ;
-let b = "FF";
+// dish1[a] = false;
+// // dish1.vegetarian = false;
+// dish2.vegetarian = true;
+// console.log(dish1);
+// console.log(dish2);
+//-----------------------------------------------------------------------------------------------
+// กำหนดให้ Variable blueColor มีค่าเท่ากับ Value ลำดับที่ 3 ของ Array colors
+// กำหนดให้ Variable fourthColor มีค่าเท่ากับ Value ลำดับที่ 4 ของ Array colors
+// กำหนดให้ Variable fifthColor มีค่าเท่ากับ Value ลำดับที่ 5 ของ Array colors
+// กำหนดให้ Variable tenthColor มีค่าเท่ากับ Value ลำดับที่ 10 ของ Array colors
+// จากนั้นให้ console.log ค่าที่อยู่ใน Variable blueColor, fourthColor, fifthColor และ tenthColor ออกมาทางหน้าจอแล้วสังเกตผลลัพธ์
+// let colors = ["red", "green", "blue", "khaki", "orange"];
+// Start coding here
+// let blueColor = colors[2];
+// let fourthColor = colors[3] 
+// let fifthColor = colors[4]
+// let tenthColor = colors[10]
 
-AA[a] = "JJ";
-console.log(AA)
+// console.log(blueColor);
+// console.log(fourthColor);
+// console.log(fifthColor);
+// console.log(tenthColor);
+//-----------------------------------------------------------------------------------------------
+// กำหนดให้ Variable alex มีค่าเท่ากับ Value ที่มาจาก Key name ของพนักงานที่ชื่อ Alex
+// กำหนดให้ Variable alexHobbies มีค่าเท่ากับ Value ที่มาจาก Key hobbies ของพนักงานที่ชื่อ Alex
+// กำหนดให้ Variable alexAndJamesAge มีค่าเท่ากับอายุของ Alex และ James รวมกัน
+// จากนั้นให้ console.log ค่าที่อยู่ใน Variable alex, alexHobbies และ alexAndJamesAge ออกมาทางหน้าจอแล้วสังเกตผลลัพธ์
+// const employees = [
+//     {
+//         name: "James",
+//         age: 40,
+//         hobbies: ["Football", "Social media"],
+//     },
+//     {
+//       name: "Alex",
+//       age: 25,
+//       hobbies: ["Cryptocurrency", "Social media"],
+//     },
+// ];
+// // Start coding here
+// let alex = employees[1].name;
+// let alexHobbies = employees[1].hobbies;
+// let alexAndJamesAge = employees[0].age + employees[1].age
+
+// console.log(alex);
+// console.log(alexHobbies);
+// console.log(alexAndJamesAge);
+//-----------------------------------------------------------------------------------------------
+// จากโค้ดใน Exercise #2 ให้เขียนโค้ดตามโจทย์ที่กำหนด
+// ให้เพิ่มข้อมูลของพนักงาน 2 คนที่อยู่ในรูปแบบของ Object เข้าไปใน Array ที่ชื่อ employees
+// เมื่อเพิ่มพนักงานเสร็จแล้ว ให้ console.log เพื่อดูค่าของ Array employees
+// ซึ่ง Object จะมี Property ดังนี้
+// พนักงานคนที่ 1 มี Property ของ Object ดังนี้
+// Key name มี Value เป็น "Alicia"
+// Key age มี Value เป็น 29
+// Key hobbies มี Value เป็น “Shopping" และ “Reading novels"
+// พนักงานคนที่ 2 มี Property ของ Object ดังนี้
+// Key name มี Value เป็น "Kody"
+// Key age มี Value เป็น 19
+// Key hobbies มี Value เป็น “Computer games" และ “Wakeboard"
+
+// const employees = [
+//     {
+//         name: "James",
+//         age: 40,
+//         hobbies: ["Football", "Social media"],
+//     },
+//     {
+//       name: "Alex",
+//       age: 25,
+//       hobbies: ["Cryptocurrency", "Social media"],
+//     },
+// ];
+
+// employees.push(
+//     {name: "Alicia",
+//         age: 29,
+//         hobbies: ["Shopping", "Reading novels"] },
+//     {name: "Kody",
+//         age: 19,
+//         hobbies: ["Computer games" , "Wakeboard"]
+
+//     }
+// )
+// console.log(employees);
+//----------------------------------------------------------------------------------
+// const student = {
+//       name: "John Doe",
+//       age: 18,
+//       grade: 12,
+//       subjects: ["Math", "Science", "History"],
+//     };
+//     console.log("Student Name: " + student.name);
+//     console.log("Student Age: " + student.age);
+//     console.log("Student Grade: " + student.grade);
+//     console.log("Student Subjects: " + student.subjects);
+//--------------------------------------------------------------------------------------------------------------
+// กำหนดให้ Variable isArray มีค่าเท่ากับ ผลลัพธ์ที่ได้จากการตรวจสอบว่า Array orders เป็น Array จริงๆ หรือไม่
+// กำหนดให้ Variable creditCardTypeOfBlindermann มีค่าเท่ากับ Value ที่มาจาก Key creditCardType ของลูกค้าชื่อ Toinette Blindermann
+// ให้ Reassign ค่าของ Key creditCardType ของลูกค้าชื่อ Toinette Blindermann ให้เป็น “visa"
+// กำหนดให้ Variable productQuantityOfJoannet มีค่าเป็น Value ที่มาจาก Key productQuantity ของลูกค้าชื่อ Anjela Joannet
+// กำหนดให้ Variable totalPurchaseOfDary มีค่าเป็น Value เกิดจากการคำนวนยอดซื้อสินค้ารวมของลูกค้าชื่อ Celia Dary
+// 🕵🏼‍♀️  Hint: ยอดซื้อสินค้ารวมหมายถึง ราคาสินค้า x จำนวนสินค้า
+// ให้ลบ Object ของลูกค้าชื่อ Brenn Hugk ที่อยู่ใน Array orders
+// เมื่อลบ Object เสร็จแล้วให้ทำการแสดงผลค่าของ orders ออกมาดูทางหน้าจอ
+let orders = [
+    {
+      id: 1,
+      customerName: "Brenn Hugk",
+      productName: "Chocolate - Milk, Callets",
+      productPrice: 94690,
+      productQuantity: 35,
+      creditCardType: "solo",
+    },
+    {
+      id: 2,
+      customerName: "Celia Dary",
+      productName: "Bread - Pumpernickle, Rounds",
+      productPrice: 10746,
+      productQuantity: 87,
+      creditCardType: "jcb",
+    },
+    {
+      id: 3,
+      customerName: "Toinette Blindermann",
+      productName: "Bar - Granola Trail Mix Fruit Nut",
+      productPrice: 94191,
+      productQuantity: 31,
+      creditCardType: "switch",
+    },
+    {
+      id: 4,
+      customerName: "Anjela Joannet",
+      productName: "Cheese - Parmesan Cubes",
+      productPrice: 5131,
+      productQuantity: 96,
+      creditCardType: "diners-club-carte-blanche",
+    },
+    {
+      id: 5,
+      customerName: "Kennith Bussons",
+      productName: "Wine - White, Pinot Grigio",
+      productPrice: 94432,
+      productQuantity: 75,
+      creditCardType: "jcb",
+    },
+];
+// Start coding here
+let isArray = Array.isArray(orders)
+console.log(isArray)
+let creditCardTypeOfBlindermann = orders[2].creditCardType;
+orders[2].creditCardType = "visa";
+let productQuantityOfJoannet = orders[3].productQuantity;
+let totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity;
+orders.shift();
+
+console.log(creditCardTypeOfBlindermann)
+console.log(productQuantityOfJoannet)
+console.log(totalPurchaseOfDary)
+console.log(orders)
